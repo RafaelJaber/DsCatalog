1fea93651aa6dfd3c83d2084295ce033a7fe9aba
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-15 12:55:16 -0300

    ✨ (CategoryController.java): Adiciona endpoint para inserir uma nova categoria 📝 (CategoryRequest.java): Adiciona DTO para representar a requisição de categoria 📝 (CategoryService.java): Adiciona lógica para inserir uma nova categoria
    O commit introduz um novo endpoint no controlador de categoria para permitir a inserção de uma nova categoria. Além disso, foi adicionado um DTO para representar a requisição de categoria e a lógica necessária no serviço para inserir a categoria no banco de dados. Essas mudanças visam melhorar a funcionalidade do sistema, permitindo a adição de novas categorias de forma mais estruturada e eficiente.

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,14 +1,14 @@
 package com.devsuperior.dscatalog.controllers;
 
+import com.devsuperior.dscatalog.dto.requests.CategoryRequest;
 import com.devsuperior.dscatalog.dto.responses.CategoryResponse;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +32,13 @@ public class CategoryController {
     public ResponseEntity<CategoryResponse> findById(@PathVariable Long id) {
         CategoryResponse categoryResponse = categoryService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(categoryResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> insert(@RequestBody CategoryRequest categoryRequest) {
+        CategoryResponse inserted = categoryService.insert(categoryRequest);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(inserted.getId()).toUri();
+        return ResponseEntity.created(uri).body(inserted);
     }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.requests.CategoryRequest;
 import com.devsuperior.dscatalog.dto.responses.CategoryResponse;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -7,6 +8,7 @@ import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Service
@@ -33,5 +35,20 @@ public class CategoryService {
                 () -> new EntityNotFoundException("Category", "id", id.toString())
         );
         return new CategoryResponse(category);
+    }
+
+    @Transactional
+    public CategoryResponse insert(CategoryRequest request) {
+        Category category = getCategory(request);
+        category = categoryRepository.save(category);
+        return new CategoryResponse(category);
+    }
+
+
+    private static Category getCategory(CategoryRequest request) {
+        return Category.builder()
+                .name(request.getName())
+                .createdAt(OffsetDateTime.now())
+                .build();
     }
 }
