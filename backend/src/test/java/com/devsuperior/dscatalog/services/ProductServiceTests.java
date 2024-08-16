b90b950d53851fd80d274855f54c1d3d7d084316
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-16 11:15:46 -0300

    🔧 (ProductServiceTests.java): Adiciona novos métodos de teste e configurações ✨ (ProductServiceTests.java): Adiciona novos métodos de teste para ProductService Os novos métodos de teste foram adicionados para testar as funcionalidades do ProductService, como encontrar todos os produtos paginados, encontrar um produto por ID, inserir um novo produto, atualizar um produto existente e excluir um produto. Além disso, foram feitas configurações adicionais para simular o comportamento dos repositórios de produtos e categorias durante os testes.

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,17 +1,31 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.requests.ProductRequest;
+import com.devsuperior.dscatalog.dto.responses.ProductResponse;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseIntegrityException;
 import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -22,16 +36,38 @@ public class ProductServiceTests {
     @Mock
     private ProductRepository productRepository;
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
     private Long existingId;
     private Long nonExistingId;
     private Long dependentId;
+    private PageImpl<Product> page;
+    private Product product;
+    private Category category;
 
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
-        nonExistingId = 2L;
+        nonExistingId = 3L;
         dependentId = 4L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
+        category = Factory.createCategory();
+
+        Mockito.when(productRepository.findAll(ArgumentMatchers.any(Pageable.class))).thenReturn(page);
+
+        Mockito.when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
+        Mockito.when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        Mockito.when(productRepository.save(ArgumentMatchers.any(Product.class))).thenReturn(product);
+
+        Mockito.when(productRepository.getReferenceById(existingId)).thenReturn(product);
+        Mockito.when(productRepository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.when(categoryRepository.getReferenceById(existingId)).thenReturn(category);
+        Mockito.when(categoryRepository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
         Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
@@ -40,6 +76,63 @@ public class ProductServiceTests {
         Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
         Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
     }
+
+    @Test
+    public void findAllPagedShouldReturnPage() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<ProductResponse> result = productService.findAll(pageable);
+
+        Assertions.assertNotNull(result);
+        Mockito.verify(productRepository, Mockito.times(1)).findAll(pageable);
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenExistingId() {
+        ProductResponse result = productService.findById(existingId);
+
+        Assertions.assertNotNull(result);
+        Mockito.verify(productRepository, Mockito.times(1)).findById(existingId);
+    }
+
+    @Test
+    public void findByIdShouldThrowsExceptionWhenNonExistingId() {
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            productService.findById(nonExistingId);
+        });
+        Mockito.verify(productRepository, Mockito.times(1)).findById(nonExistingId);
+    }
+
+    @Test
+    public void insertShouldBeAbleToPersistProduct() {
+        Assertions.assertDoesNotThrow(() -> {
+            ProductRequest request = Factory.createProductRequest();
+            productService.insert(request);
+        });
+        Mockito.verify(productRepository, Mockito.times(1)).save(ArgumentMatchers.any(Product.class));
+    }
+
+    @Test
+    public void updateShouldBeReturnProductResponseWhenIdExists() {
+        ProductRequest productRequest = Factory.createProductRequest();
+
+        ProductResponse productResponse = productService.update(productRequest, existingId);
+
+        Assertions.assertNotNull(productResponse);
+        Mockito.verify(productRepository, Mockito.times(1)).getReferenceById(existingId);
+    }
+
+    @Test
+    public void updateShouldBeThrowsExceptionWhenIdDoesNotExist() {
+        ProductRequest productRequest = Factory.createProductRequest();
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            productService.update(productRequest, nonExistingId);
+        });
+
+        Mockito.verify(productRepository, Mockito.times(1)).getReferenceById(nonExistingId);
+    }
+
 
     @Test
     public void deleteShouldDoNothingWhenIdExists() {
