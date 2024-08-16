b4288fd44a42fe0286208fa646a0d1b4490f1f35
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-16 01:23:02 -0300

    🔧 (ProductServiceTests.java): Adiciona exceção de integridade de banco de dados e tratamento ✨ (ProductServiceTests.java): Adiciona teste para verificar se a exceção de integridade de banco de dados é lançada ao excluir um produto com dependências A exceção de integridade de banco de dados e seu tratamento foram adicionados para lidar com a exclusão de produtos que possuem dependências. Um novo teste foi adicionado para garantir que a exceção correta seja lançada ao tentar excluir um produto com dependências, garantindo a integridade dos dados no banco de dados.

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseIntegrityException;
 import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -22,15 +24,21 @@ public class ProductServiceTests {
 
     private Long existingId;
     private Long nonExistingId;
+    private Long dependentId;
 
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
         nonExistingId = 2L;
+        dependentId = 4L;
+
+        Mockito.doNothing().when(productRepository).deleteById(existingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
 
         Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
         Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
     }
 
     @Test
@@ -43,11 +51,21 @@ public class ProductServiceTests {
     }
 
     @Test
-    public void deleteShouldThrowExceptionWhenIdDoesNotExist() {
+    public void deleteShouldThrowEntityNotFoundExceptionWhenIdDoesNotExist() {
         Assertions.assertThrows(EntityNotFoundException.class, () -> {
             productService.deleteById(nonExistingId);
         });
 
         Mockito.verify(productRepository, Mockito.never()).deleteById(nonExistingId);
     }
+
+    @Test
+    public void deleteShouldThrowDatabaseIntegrityExceptionWhenDependentId() {
+        Assertions.assertThrows(DatabaseIntegrityException.class, () -> {
+            productService.deleteById(dependentId);
+        });
+
+        Mockito.verify(productRepository).deleteById(dependentId);
+    }
+
 }
