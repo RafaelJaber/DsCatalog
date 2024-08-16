e456d4ebb2b261d944e150c025cf8ff2cb977a2c
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-16 19:11:00 -0300

    ✨ (DscatalogApplicationIT.java): Adiciona teste de integração para carregamento bem-sucedido do contexto da aplicação Este commit adiciona um novo arquivo de teste de integração para garantir que o contexto da aplicação seja carregado com sucesso. O teste verifica se a aplicação pode ser inicializada corretamente, o que é crucial para garantir o funcionamento adequado da aplicação.

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Tag("Integration")
-class DscatalogApplicationTests {
+class DscatalogApplicationIT {
 
     @Test
     @DisplayName("Should load application context successfully")
