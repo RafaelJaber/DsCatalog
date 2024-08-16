cb288fabc169de9afe25ea553825f1becf98d049
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-15 21:32:46 -0300

    ✨ (Product.java): Adiciona a entidade Product com seus atributos e relacionamentos A entidade Product foi adicionada ao projeto com seus atributos e relacionamentos necessários para representar um produto. Isso permitirá a manipulação e persistência de informações sobre produtos no sistema.
    ✨ (import.sql): Adiciona novos produtos à tabela tb_product
    Os novos produtos foram adicionados à tabela tb_product no arquivo import.sql para atualizar o banco de dados com os novos registros de produtos.
    
    ✨ Adiciona novos produtos à tabela tb_product e suas respectivas categorias
    Os novos produtos e suas informações foram inseridos na tabela tb_product, juntamente com suas categorias correspondentes na tabela tb_product_category. Isso amplia a variedade de produtos disponíveis no sistema e os associa às categorias apropriadas para facilitar a organização e a busca.
    
    ✨ Adiciona 15 novos registros de produtos na tabela tb_product
    Os novos registros de produtos foram adicionados à tabela tb_product com informações como nome, preço, data, descrição e URL da imagem. Isso foi feito para expandir a variedade de produtos disponíveis no sistema.
    
    ✨ Adiciona novos registros de produtos e suas categorias na base de dados
    Os comandos SQL adicionam novos registros de produtos e suas respectivas categorias na base de dados. Isso é feito para atualizar o catálogo de produtos disponíveis no sistema e associá-los às categorias corretas, permitindo uma melhor organização e filtragem dos produtos para os usuários.

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private Double price;
+
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private OffsetDateTime date;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns =  @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
