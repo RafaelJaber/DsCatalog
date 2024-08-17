c7dd3593f6455ea8f2dfd7ee0669ecd0735c50f5
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-16 23:45:22 -0300

    ✨ (Role.java, User.java, import.sql): Adiciona entidades Role e User com relacionamento Many-to-Many e scripts SQL para inserção de dados iniciais As entidades Role e User foram adicionadas ao projeto, permitindo a representação de papéis e usuários. Foi estabelecido um relacionamento Many-to-Many entre as entidades User e Role, possibilitando que um usuário possua múltiplos papéis e vice-versa. Além disso, foram incluídos scripts SQL no arquivo import.sql para inserir dados iniciais de usuários, papéis e a associação entre eles. Essas mudanças visam implementar a lógica de controle de acesso e autenticação no sistema.

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
